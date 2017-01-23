d52fd26682b3d10e94a93e52db2129d35511f19e
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2017-01-23 15:48:13 -0500

    added three database models

--- src/audemes/main/admin.py
+++ src/audemes/main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import New
+from .models import *
 # Register your models here.
 
 admin.site.register(New)
+admin.site.register(GameAtomicAudeme)
+admin.site.register(GameComplexAudeme)
+admin.site.register(Game)
--- src/audemes/main/models.py
+++ src/audemes/main/models.py
@@ -10,3 +10,27 @@ class New(models.Model):
 
     def __str__(self):
         return self.author + ' : ' + self.subject
+    
+class GameAtomicAudeme(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+        
+    def __str__(self):
+        return question_text
+
+
+class GameComplexAudeme(models.Model):
+    question = models.ForeignKey(GameAtomicAudeme, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)  
+    
+    def __str__(self):
+        return self.choice_text  
+    
+class Game(models.Model):
+    question = models.ForeignKey(GameComplexAudeme, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    numAtomicAudemes = models.IntegerField(default=0)  
+    
+    def __str__(self):
+        return self.choice_text     
