0a1760b9c93a14a42a8b0805e7c73375dd6b8d6c
rluttmeratius <rebecca.luttmer@gmail.com>
2017-03-06 20:24:11 +0000

    csv and sql dictionary files

---
+++ html/search/dictionary.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `dictionary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dictionary` (
+  `name` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `keywords` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOAD DATA INFILE '/var/www/Audemes/html/search/dictionary.csv'  
+INTO TABLE dictionary
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
