4ee9bcbd820aa1cc652653db1ef4ae7e9d70c8e5
rluttmeratius <rebecca.luttmer@gmail.com>
2017-03-06 22:12:58 +0000

    worked on search

--- html/search/dictionary.php
+++ html/search/dictionary.php
@@ -11,22 +11,7 @@
     <link href="../css/homeStyle.css" rel="stylesheet">
     <link href="../css/dicStyle.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/playerStyles.css"/>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <script type="text/javascript" src="../js/amplitude.js"></script>
-    <script type="text/javascript" src="app.js"></script>
-
-    <style>
-        .paginate > li {
-            float: left;
-            display: block;
-            margin: 10px;
-        }
-    </style>
 </head>
 <body>
 <nav class="navbar navbar-default" role="navigation">
@@ -44,10 +29,9 @@
         </ul>
     </div>
 </nav>
-
-<div class="container-fluid search-area show-results" tabitem="0">
+<h1>Search Audemes</h1>
     <div class="row" id="advance">
-        <div class="col-md-2 col-md-offset-1">
+        <div class="col-md-4">
             <p>Category: </p>
             <select id="category">
                 <option value="0">All</option>
@@ -62,81 +46,73 @@
                 <option value="9">Physics</option>
             </select>
         </div>
-        <div class="col-md-2">
-            <p>Difficulty: </p>
-            <select id="difficulty">
-                <option value="0">All</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-        </div>
-        <div class="col-md-2">
-            <p>Show atomics only: </p>
-            <form id="atomic">
-                <input type="radio" name="atomic" value="1">Yes</input><span> </span>
-                <input type="radio" name="atomic" value="0" checked>No</input>
-            </form>
-        </div>
-        <div class="col-md-2">
+        <div class="col-md-4">
             <p>Word or phrase: </p>
             <div class="search-wrapper">
                 <input id="search" type="text"/>
             </div>
         </div>
 
-        <div class="col-md-2">
-            <input id="submit" type="submit" value="Search" onclick=search()>
+        <div class="col-md-4">
+        <p>Search:</p>
+            <input id="submit" type="submit" value="Go" onclick=search()>
         </div>
     </div>
-</div>
+    <br>
 <?php
-$allfiles = scandir('../audio/');
-$files = array_diff($allfiles, array('.', '..'));
-echo '<ul>';
-foreach($files as $file) {
+include '/home/phpcredentials/access.php';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-    echo '<li>';
-    $filename = explode("." , $file);
-    $fileonly = $filename[0];
-    echo '<audio id="' . $fileonly . '" src="http://audemes.aphtech.org/audio/' . $file . '" preload="auto"></audio>' ;
-    echo '<button type="submit" onclick="document.getElementById(\''. $fileonly .'\').play();">' . $file . '</button>';
-    echo '</li>';
-    echo '<br>';
+$sql = "SELECT * FROM dictionary";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    $name = $row["name"];
+    $description = $row["description"];
+    $keywords = $row["keywords"];
+    $category = $row["category"];
+    echo '<div class="row">';
+               
+    echo '<div class="col-md-3">';
+    echo '<audio id="' . $name . '" src="http://audemes.aphtech.org/audio/' . strtolower($name) . '.mp3" preload="auto"></audio>' ;
+    echo '<button type="submit" onclick="document.getElementById(\''. $name .'\').play();">' . $name . '</button>';
+    echo '</div>';
+    
+    echo '<div class="col-md-3">';
+    echo '<p>';
+    echo '<strong> Category: </strong>';
+    echo $category;
+    echo '</p>';
+    echo '</div>';
+    
+    echo '<div class="col-md-3">';
+    echo '<p>';
+    echo '<strong>Description:</strong>';
+    echo $description;
+    echo '</p>';
+    echo '</div>';
+    
+    echo '<div class="col-md-3">';
+    echo '<p>';
+    echo '<strong>Keywords:</strong>';
+    echo $keywords;
+    echo '</p>';
+    echo '</div>';
+    
+    echo '</div>';
+    }
+} else {
+    echo "0 results";
 }
-echo '</ul>';
+$conn->close();
+
 ?>
-<!--
-<div class="resultpanel border">
-<h4>Air (ver. 1)</h4>
-<h5>Description</h5>
-<p>The invisible gaseous substance surrounding the earth, a mixture mainly of oxygen and nitrogen.	</p>
-<h5>Category</h5>
-<p></p>
-<h5>Tags</h5>
-<ul>
-	<li></li>
-	<li></li>
-	<li></li>
-</ul>
-<audio controls>
-	<source src="../AudemeOGGs/air 1.ogg" type="audio/ogg">
-	<source src="../AudemeMP3s/air 1.mp3" type="audio/mpeg">
-</audio>
-</div>
-<h4 id="results-text">Showing results for: <strong id="search-string"></strong></h4>-->
-<div class="content-fluid">
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3" id="results">
-
-        </div>
-    </div>
-</div>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="../js_2014/jquery-1.11.2.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="../js/bootstrap.min.js"></script>
-<script src="app.js"></script>
 </body>
 </html>
