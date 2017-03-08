60f965cc99ff66d1b6b0227cb7c994a99db5e4ef
rluttmeratius <rebecca.luttmer@gmail.com>
2017-03-08 23:01:50 +0000

    functionality to search by category

--- html/search/dictionary.php
+++ html/search/dictionary.php
@@ -9,9 +9,8 @@
     <!-- Bootstrap -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/homeStyle.css" rel="stylesheet">
-    <link href="../css/dicStyle.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/playerStyles.css"/>
-    <script type="text/javascript" src="../js/amplitude.js"></script>
+
 </head>
 <body>
 <nav class="navbar navbar-default" role="navigation">
@@ -30,14 +29,15 @@
     </div>
 </nav>
 <h1>Search Audemes</h1>
-    <div class="row" id="advance">
+<div class="row" id="advance">
+    <form method="post">
         <div class="col-md-4">
             <p>Category: </p>
-            <select id="category">
-                <option value="0">All</option>
-                <option value="1">Applied Science</option>
-                <option value="2">Astronomy</option>
-                <option value="3">Biology</option>
+            <select id="category" name="catname">
+                <option value="All">All</option>
+                <option value="Applied Science">Applied Science</option>
+                <option value="Astronomy">Astronomy</option>
+                <option value="BIOLOGY">Biology</option>
                 <option value="4">Earth Science</option>
                 <option value="5">Energy</option>
                 <option value="6">General</option>
@@ -49,70 +49,96 @@
         <div class="col-md-4">
             <p>Word or phrase: </p>
             <div class="search-wrapper">
-                <input id="search" type="text"/>
+                <input id="search" name="search" type="text"/>
             </div>
         </div>
 
         <div class="col-md-4">
-        <p>Search:</p>
-            <input id="submit" type="submit" value="Go" onclick=search()>
+            <p>Search:</p>
+                       <input id="submit" type="submit" value="Go">
         </div>
-    </div>
-    <br>
+    </form>
+</div>
+<br>
 <?php
-include '/home/phpcredentials/access.php';
+
+function showSearch($catname, $string)
+{
+    include '/home/phpcredentials/access.php';
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT * FROM dictionary";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-    $name = $row["name"];
-    $description = $row["description"];
-    $keywords = $row["keywords"];
-    $category = $row["category"];
-    echo '<div class="row">';
-               
-    echo '<div class="col-md-3">';
-    echo '<audio id="' . $name . '" src="http://audemes.aphtech.org/audio/' . strtolower($name) . '.mp3" preload="auto"></audio>' ;
-    echo '<button type="submit" onclick="document.getElementById(\''. $name .'\').play();">' . $name . '</button>';
-    echo '</div>';
-    
-    echo '<div class="col-md-3">';
-    echo '<p>';
-    echo '<strong> Category: </strong>';
-    echo $category;
-    echo '</p>';
-    echo '</div>';
-    
-    echo '<div class="col-md-3">';
-    echo '<p>';
-    echo '<strong>Description: </strong>';
-    echo $description;
-    echo '</p>';
-    echo '</div>';
-    
-    echo '<div class="col-md-3">';
-    echo '<p>';
-    echo '<strong>Keywords: </strong>';
-    echo $keywords;
-    echo '</p>';
-    echo '</div>';
-    
-    echo '</div>';
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-} else {
-    echo "0 results";
-}
-$conn->close();
+    $selectedCategory = $_POST['category'];
+    $sql;
+    if ($catname == 'All') {
+        if ($string == ""){
+            $sql = "SELECT * FROM dictionary";
+        }
+        else{
+            $sql = "SELECT * FROM dictionary WHERE ";
+        }
+    } else {
+        if ($string == ""){
+            $sql = "SELECT * FROM dictionary WHERE category = '" . $catname . "'";
+        }
+        else {
+            $sql = "SELECT * FROM dictionary WHERE category = '" . $catname . "'";
+        }
 
+    }
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $name = $row["name"];
+            $description = $row["description"];
+            $keywords = $row["keywords"];
+            $category = $row["category"];
+            echo '<div class="row">';
+
+            echo '<div class="col-md-3">';
+            echo '<audio id="' . $name . '" src="http://audemes.aphtech.org/audio/' . strtolower($name) . '.mp3" preload="auto"></audio>';
+            echo '<button type="submit" onclick="document.getElementById(\'' . $name . '\').play();">' . $name . '</button>';
+            echo '</div>';
+
+            echo '<div class="col-md-3">';
+            echo '<p>';
+            echo '<strong> Category: </strong>';
+            echo $category;
+            echo '</p>';
+            echo '</div>';
+
+            echo '<div class="col-md-3">';
+            echo '<p>';
+            echo '<strong>Description: </strong>';
+            echo $description;
+            echo '</p>';
+            echo '</div>';
+
+            echo '<div class="col-md-3">';
+            echo '<p>';
+            echo '<strong>Keywords: </strong>';
+            echo $keywords;
+            echo '</p>';
+            echo '</div>';
+
+            echo '</div>';
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+}
+
+if (isset($_POST['catname'])) {
+    var_dump($_POST);
+    showSearch($_POST['catname'], $_POST['search']);
+}
 ?>
 </body>
 </html>
